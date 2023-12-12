5aea8966f8155fef82a3e219facf55fb79e6660a
Sulan Adam <sulan.adam19@gmail.com>
2023-12-12 16:45:23 +0100

    alkalmazottak

---
+++ src/app/base.service.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseService {
+  empUrl = "https://localhost:7049/api/Employees/"
+  depUrl = "https://localhost:7049/api/Departments/"
+
+  private employeeSub = new Subject()
+  private departmenteSub = new Subject()
+  constructor(private http: HttpClient) {
+    this.loadEmployees()
+    this.loadDepartments()
+  }
+
+
+  loadEmployees() {
+    this.http.get(this.empUrl).subscribe(
+      (res) => this.employeeSub.next(res)
+    )
+  }
+
+  addEmployee(body: any) {
+    console.log(body)
+    this.http.post(this.empUrl, body)
+  }
+  UpdateEmployee(body: any) {
+    this.http.put(this.empUrl + body.EmployeeId, body)
+  }
+  deleteEmployee(body: any) {
+    this.http.delete(this.empUrl + body.EmployeeId)
+  }
+  getEmployees() {
+    return this.employeeSub
+  }
+
+  
+  loadDepartments() {
+    this.http.get(this.depUrl).subscribe(
+      (res) => this.departmenteSub.next(res)
+    )
+  }
+
+  addDepartment(body: any) {
+    this.http.post(this.depUrl, body)
+  }
+  UpdateDepartment(body: any) {
+    this.http.put(this.depUrl + body.Id, body)
+  }
+  deleteDepartment(body: any) {
+    this.http.delete(this.depUrl + body.Id)
+  }
+  getDepartments() {
+    return this.departmenteSub
+  }
+}
---
+++ src/app/employees-list/employees-list.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+import { BaseService } from '../base.service';
+
+@Component({
+  selector: 'app-employees-list',
+  templateUrl: './employees-list.component.html',
+  styleUrls: ['./employees-list.component.css']
+})
+export class EmployeesListComponent {
+
+  newEmployee:any={}
+  empList:any
+  depList:any
+  columns:any=[
+    {key:"employeeId", id:1, text:"Id", type:"plain"},
+    {key:"employeeName", id:2, text:"Name", type:"text"},
+    {key:"departmentName", id:3, text:"Department", type:"selection"},
+  ]
+
+  constructor(private base:BaseService){
+    this.base.getEmployees().subscribe(
+      (res)=> this.empList=res
+    )
+    this.base.getDepartments().subscribe(
+      (res)=>this.depList=res
+    )
+  }
+
+  addEmployee(){
+    this.base.addEmployee(this.newEmployee)
+  }
+
+  updateEmployee(body:any){
+    this.base.UpdateEmployee(body)
+  }
+
+  terminateEmployee(body:any){
+    this.base.deleteEmployee(body)
+  }
+
+
+}
